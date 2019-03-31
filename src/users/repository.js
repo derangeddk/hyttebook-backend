70d19bbd46c1060433e8457b3bfc2c70a4f94c28
ITViking <philip.dein@gmail.com>
2019-03-31 23:41:29 +0200

    Ensure upon app start, that a users table exists with an admin user If no table exists, then create one

--- src/users/repository.js
+++ src/users/repository.js
@@ -1,19 +1,85 @@
 const uuid = require('uuid');
 
 module.exports = (db) => {
-    //TODO: ensure users table exists
+    ensureUsersTableExists(db);
 
     return {
-        create: async (username, password) => {
-            let id = uuid.v4();
-            let now = (new Date()).toISOString();
-            let passwordHash = 1;
-            let salt = 1;
-            await db.query("INSERT INTO users (id, username, passwordHash, salt, data) VALUES ($1::uuid, $2::text, $3::text, $4::text, $5::json)", [
-                id, username, passwordHash, salt, { createdAt: now, updatedAt: now }
-            ]);
-
-            return { id, username };
-        },
+        create: createUser,
     };
 };
+
+async function ensureUsersTableExists(db) {
+    if (await tableExists(db)){
+        return;
+    }
+
+    try{
+        await db.query(
+            `CREATE TABLE users(
+                id uuid PRIMARY KEY,
+                username text UNIQUE NOT NULL,
+                passwordHash text NOT NULL,
+                salt text NOT NULL,
+                data json NOT NULL
+            )`
+        );
+
+        await createUser(db, "admin","admin");
+
+    } catch(error) {
+        console.error("Something went wrong upon creating the users table", error);
+        process.exit(1);
+    }
+}
+
+async function tableExists(db) {
+    let result = await db.query(
+        `SELECT 'public.users'::regclass`
+    )
+    .then(() => {
+        return true;
+    })
+    .catch(error => {
+        return false;
+    })
+
+    return result;
+}
+
+async function createUser(db, username, password) {
+    let id = uuid.v4();
+    let now = (new Date()).toISOString();
+    let passwordHash = 1;
+    let salt = 1;
+    await db.query(
+        `INSERT INTO users (
+            id,
+            username,
+            passwordHash,
+            salt,
+            data
+            )
+        VALUES (
+            $1::uuid,
+            $2::text,
+            $3::text,
+            $4::text,
+            $5::json
+        )`,
+        [
+            id,
+            username,
+            passwordHash,
+            salt,
+            {
+                createdAt: now,
+                updatedAt: now
+            }
+        ]
+    )
+    .catch((error) => {
+        console.log("something went wrong upon inserting an admin user: ", error.message);
+    });
+
+    return { id, username };
+}
