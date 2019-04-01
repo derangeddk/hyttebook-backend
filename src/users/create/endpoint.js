a80b51d92d4114aa405cc2f18af23f42b026d68d
ITViking <philip.dein@gmail.com>
2019-04-01 17:28:21 +0200

    endpoint validates username, full name and email

--- src/users/create/endpoint.js
+++ src/users/create/endpoint.js
@@ -1,16 +1,20 @@
 module.exports = (users) => async (req, res) => {
-    let { hutName, firstName, lastName, email, password } = req.body;
+    let { hutName, fullName, username, email, password } = req.body;
 
     let requestErrors = {
         errorCount: 0,
-        hutName: []
+        hutName: [],
+        fullName: [],
+        username: [],
+        email: [],
+        password: []
     };
 
     hutName = validateHutName(hutName, requestErrors);
 
-    firstName = validateFirstName(firstName, requestErrors);
+    fullName = validateFullName(fullName, requestErrors);
 
-    lastName = validateLastName(lastName, requestErrors);
+    username = validateUsername(username, requestErrors);
 
     email = validateEmail(email, requestErrors);
 
@@ -49,15 +53,78 @@ function validateHutName(hutName, requestErrors) {
     return hutName;
 }
 
-function validateFirstName(firstName, requestErrors) {
+function validateFullName(fullName, requestErrors) {
+    if(!fullName) {
+        requestErrors.fullName.push({
+            code: "MISSING",
+            da: "Indtast venligst et navn"
+        });
+        requestErrors.errorCount++;
+        return;
+    }
 
+    fullName = fullName.trim();
+
+    if(fullName.length < 1) {
+        requestErrors.fullName.push({
+            code: "TOO SHORT",
+            da: "Venligst indtast et længere navn",
+            value: fullName
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+
+    return fullName;
 }
 
-function validateLastName(lastName, requestErrors) {
+function validateUsername(username, requestErrors) {
+    if(!username) {
+        requestErrors.username.push({
+            code: "MISSING",
+            da: "Indtast venligst et brugernavn"
+        });
+        requestErrors.errorCount++;
+        return;
+    }
 
+    username = username.trim();
+
+    if(username.length < 1) {
+        requestErrors.username.push({
+            code: "TOO SHORT",
+            da: "Venligst indtast et længere brugernavn",
+            value: username
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+
+    return username;
 }
 
 function validateEmail(email, requestErrors) {
+    if(!email) {
+        requestErrors.email.push({
+            code: "MISSING",
+            da: "Indtast venligst en email"
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+
+    email = email.trim();
+
+    if(!email.match(/^.+@.+$/)) {
+        requestErrors.email.push({
+            code: "INVALID FORMAT",
+            da: "Email formatet var ikke overholdt, prøv igen",
+            value: email
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+
     return email;
 }
 
