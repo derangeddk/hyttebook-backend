b3219085c47ef804948a29b5923c621ecdeae2f4
ITViking <philip.dein@gmail.com>
2019-04-01 16:33:31 +0200

    endpoint validates hut name

--- src/users/create/endpoint.js
+++ src/users/create/endpoint.js
@@ -1,14 +1,66 @@
 module.exports = (users) => async (req, res) => {
-    let { username, password } = req.body;
+    let { hutName, firstName, lastName, email, password } = req.body;
 
-    if(!username) {
-        // ...
+    let requestErrors = {
+        errorCount: 0,
+        hutName: []
+    };
+
+    hutName = validateHutName(hutName, requestErrors);
+
+    firstName = validateFirstName(firstName, requestErrors);
+
+    lastName = validateLastName(lastName, requestErrors);
+
+    email = validateEmail(email, requestErrors);
+
+    password = validatePassword(password, requestErrors);
+
+    if(requestErrors.errorCount) {
+        res.status(400).send(requestErrors);
+        return;
     }
 
-    if(!password) {
-        // ...
-    }
+    console.log(email + ' ' + password);
 
-    let user = await users.create(username, password);
+    let user = await users.create(email, password);
     res.send(user);
 };
+
+function validateHutName(hutName, requestErrors) {
+    if(!hutName) {
+        requestErrors.hutName.push({
+            code: "MISSING",
+            da: "Angiv venligst et hytte navn"
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+    hutName = hutName.trim();
+    if(hutName.length < 3) {
+        requestErrors.hutName.push({
+            code: "TOO SHORT",
+            da: "Det indtastede hytte navn er for kort",
+            value: hutName
+        });
+        requestErrors.errorCount++;
+        return;
+    }
+    return hutName;
+}
+
+function validateFirstName(firstName, requestErrors) {
+
+}
+
+function validateLastName(lastName, requestErrors) {
+
+}
+
+function validateEmail(email, requestErrors) {
+    return email;
+}
+
+function validatePassword(password, requestErrors) {
+    return password;
+}
