4fd80b91c637d40713d14a06be44d4a4947ca561
ITViking <philip.dein@gmail.com>
2019-04-02 11:40:47 +0200

    allow cors from everywhere

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const bodyParser = require("body-parser");
 module.exports = (port, users) => {
     let app = express();
     app.use(bodyParser.json());
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
 
     app.get("/includes/:id/booking.js", (req, res) => {
         res.set("Content-Type", "application/javascript");
