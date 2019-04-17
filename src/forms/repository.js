174c7ce1d4f1ed3e603de953a11ff1a38b495f7f
ITViking <philip.dein@gmail.com>
2019-04-17 19:12:08 +0200

    repo ensures a forms table exists in the db upon starting up

--- src/forms/repository.js
+++ src/forms/repository.js
@@ -1,14 +1,40 @@
 module.exports = (db) => {
     ensureFormsTableExists(db);
     return {
-        create: ( ) => createForm(db)
+        create: (formConfigs) => createForm(db, formConfigs)
     };
 };
 
-function ensureFormsTableExists(db) {
+async function ensureFormsTableExists(db) {
+    if(await tableExists(db)) {
+        return;
+    }
+
+    try {
+        await db.query(
+            `CREATE TABLE forms(
+                id uuid UNIQUE PRIMARY KEY,
+                configuration json NOT NULL
+            )`
+        );
+    } catch(error) {
+        console.error("Something went wrong upon creating the forms table: ", error);
+        process.exit(1);
+    }
+}
+
+async function tableExists(db) {
+    try {
+        await db.query(
+            `SELECT 'public.forms'::regclass`
+        );
+    } catch(erro) {
+        return false;
+    }
+
     return true;
 }
 
-function createForm(db) {
+function createForm(db, formConfigs) {
     return true;
 }
