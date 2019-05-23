045613bdce1075c044d889c91c1586159a8fb9be
ITViking <philip.dein@gmail.com>
2019-05-23 22:23:39 +0200

    returns 400 if any argument is empty

---
+++ spec/huts/createEndpointSpec.js
@@ -0,0 +1,59 @@
+const createEndpoint = require("../../src/huts/create/endpoint");
+const theoretically = require('jasmine-theories');
+
+describe("create endpoint" , function() {
+    theoretically.it("fails if hutname has as empty string",[
+        "hutName",
+        "street",
+        "streetNumber",
+        "city",
+        "zipCode",
+        "email",
+        "phone"
+    ], async function(emptyProperty) {
+        let req = {
+            body: {
+                hutName: "a hutname for testing puposes",
+                street: "test street",
+                streetNumber: "1",
+                city: "test city",
+                zipCode: "0001",
+                email: "test@test.com",
+                phone: "12345678"
+            }
+        };
+
+        let res = {};
+
+        res.send = jasmine.createSpy("res.send").and.callFake(() => {
+            return res;
+        });
+
+        res.status = jasmine.createSpy("res.status").and.callFake(() => {
+            return res;
+        });
+
+        let newHut = { id: "test" };
+
+        let huts = {
+            create: jasmine.createSpy("huts.create").and.callFake(async () => newHut)
+        }
+
+        let endpoint = createEndpoint(huts);
+
+        req.body[emptyProperty] = "";
+
+        let actualError = null;
+        try {
+            await endpoint(req, res);
+        } catch(error) {
+            actualError = error;
+        }
+
+        expect(actualError).toBe(null);
+
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.status).toHaveBeenCalledTimes(1);
+    });
+});
--- src/huts/create/endpoint.js
+++ src/huts/create/endpoint.js
@@ -1,4 +1,31 @@
 module.exports = (huts) => async (req, res) => {
+    let {
+        hutName,
+        street,
+        streetNumber,
+        city,
+        zipCode,
+        email,
+        phone
+    } = req.body;
+
+    let requestErrors = {
+        errorCount: 0,
+    };
+
+    validateHutName(hutName, requestErrors);
+    validateStreet(street, requestErrors);
+    validateStreetNumber(streetNumber, requestErrors);
+    validateCity(city, requestErrors);
+    validateZipCode(zipCode, requestErrors);
+    validateEmail(email, requestErrors);
+    validatePhone(phone, requestErrors);
+
+    if(requestErrors.errorCount) {
+        res.status(400).send(requestErrors);
+        return;
+    }
+
     let result;
     try {
         result = await huts.create(req.body);
@@ -8,6 +35,141 @@ module.exports = (huts) => async (req, res) => {
         return;
     }
 
-    console.log(result);
     res.send(result);
 };
+
+function validateHutName(hutName, requestErrors) {
+    hutName = hutName.trim();
+    if(!hutName) {
+        requestErrors[hutName] = {
+            code: "MISSING",
+            da: "Indtast venligst hyttens navn"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+}
+
+function validateStreet(street, requestErrors) {
+    street = street.trim();
+    if(!street) {
+        requestErrors[street] = {
+            code: "MISSING",
+            da: "Indtast venligst vejnavn"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+
+
+
+    // if(street.length < 1) {
+    //     requestErrors[street] = {
+    //         code: "FORMAT",
+    //         da: "skal være længere end et bogstav"
+    //     };
+    //     requestErrors.errorCount++;
+    //     return;
+    // }
+}
+
+function validateStreetNumber(streetNumber, requestErrors) {
+    if(!streetNumber) {
+        requestErrors[streetNumber] = {
+            code: "MISSING",
+            da: "Indtast venligst vej nummeret"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+}
+
+function validateCity(city, requestErrors) {
+    city = city.trim();
+    if(!city) {
+        requestErrors[city] = {
+            code: "MISSING",
+            da: "Indtast venligst bynavn"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+
+
+
+    // if(city.length < 1) {
+    //     requestErrors[city] = {
+    //         code: "FORMAT",
+    //         da: "skal være længere end et bogstav"
+    //     };
+    //     requestErrors.errorCount++;
+    //     return;
+    // }
+}
+
+function validateZipCode(zipCode, requestErrors) {
+    if(!zipCode) {
+        requestErrors[zipCode] = {
+            code: "MISSING",
+            da: "Indtast venligst postnummeret"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+
+    zipCode = zipCode.trim();
+
+    if(!zipCode.match(/^[0-9]*$/)) {
+        requestErrors[zipCode] = {
+            code: "FORMAT",
+            da: "Må kun indeholde tal"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+}
+
+function validateEmail(email, requestErrors) {
+    if(!email) {
+        requestErrors[email] = {
+            code: "MISSING",
+            da: "Indtast venligst en email"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+
+    email = email.trim();
+
+    if(!email.match(/^.+@.+$/)) {
+        requestErrors[email] = {
+            code: "FORMAT",
+            da: "skal have en gyldig email"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+}
+
+function validatePhone(phone, requestErrors) {
+    phone = phone.trim();
+    if(!phone) {
+        requestErrors[phone] = {
+            code: "MISSING",
+            da: "Indtast venligst telefonnummer"
+        };
+        requestErrors.errorCount++;
+        return;
+    }
+
+
+
+    // if(phone.length < 1) {
+    //     requestErrors[phone] = {
+    //         code: "FORMAT",
+    //         da: "skal være længere end et tal"
+    //     };
+    //     requestErrors.errorCount++;
+    //     return;
+    // }
+}
