9a738ce420a007e19955f3656b4990047f876ac4
ITViking <philip.dein@gmail.com>
2019-06-24 23:48:19 +0200

    fixus

--- src/forms/ensureFormsTableExists.js
+++ src/forms/ensureFormsTableExists.js
@@ -7,7 +7,7 @@ module.exports = async function ensureFormsTableExists(db) {
         await db.query(
             `CREATE TABLE forms(
                 id uuid UNIQUE PRIMARY KEY,
-                hutId uuid UNIQUE
+                hutId uuid UNIQUE,
                 data json NOT NULL
             )`
         );
