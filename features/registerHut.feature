6514f45c42dd0ee167a25196fe657f7dbd997be5
ITViking <philip.dein@gmail.com>
2019-06-25 14:39:49 +0200

    extend scenario

--- features/registerHut.feature
+++ features/registerHut.feature
@@ -6,4 +6,10 @@ Feature: Register hut
         When I register a hut with the following information:
         | hutName | street      | streetNumber | city     | zipCode | email        | phone    |
         | xyz hut | Kildemarken | 96           | Havdrup  | 4622    | xyz@mail.com | 74654010 |
-        Then I should receive a response containing an id "5d539311-9701-4ab4-9074-a4f087aa5bad"
+        Then I should receive a response containing an id
+        And a hut should exist with the following information:
+        | id                | hutName | street      | streetNumber | city     | zipCode | email        | phone    |
+        | "the return uuid" | xyz hut | Kildemarken | 96           | Havdrup  | 4622    | xyz@mail.com | 74654010 |
+        And the hut should have a form
+        And I should admin of a hut named "xyz hut"
+
--- features/step_definitions/hutSteps.js
+++ features/step_definitions/hutSteps.js
@@ -1,4 +1,5 @@
-const { When } = require("cucumber");
+const { When, Then } = require("cucumber");
+const assert = require("assert");
 
 When('I register a hut with the following information:', async function (dataTable) {
     let {
@@ -11,7 +12,29 @@ When('I register a hut with the following information:', async function (dataTab
         phone
     } = dataTable.hashes()[0];
 
-
     let hutResponse = await this.client.post("/huts", { hutName, street, streetNumber, city, zipCode, email, phone });
     this.hutId = hutResponse.data;
 });
+
+Then('I should receive a response containing an id', function () {
+    this.hutId;
+});
+
+Then('a hut should exist with the following information:', async function (dataTable) {
+    let expectedHutData = dataTable.hashes();
+    let actualHutData = await this.client.get(`/huts/${this.hutId}`);
+
+    assert.deepStrictEqual(actualHutData, expectedHutData);
+});
+
+
+Then('the hut should have a form', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should admin of a hut named {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
