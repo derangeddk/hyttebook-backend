21748dc9338133a3b227bafe834216b814daf36f
ITViking <philip.dein@gmail.com>
2019-06-25 14:37:35 +0200

    make world object reflect the changes made to application start

--- features/world.js
+++ features/world.js
@@ -2,12 +2,7 @@ const { setWorldConstructor, Before, After } = require("cucumber");
 const axios = require("axios");
 const createApp = require("../src/app");
 const config = require("config")
-const { Pool } = require('pg')
-const db = new Pool(config.postgres);
-const users = require("../src/users/repository")(db);
-const forms = require("../src/forms/repository")(db);
-const huts = require("../src/huts/repository")(db);
-const app = createApp(5444, { db, users, forms, huts });
+const app = createApp(config);
 
 function World() {
     process.env.NODE_ENV = "test";
@@ -17,7 +12,7 @@ setWorldConstructor(World);
 
 Before(async function(testcase) {
     this.client = axios.create({
-        baseURL: "http://localhost:5444"
+        baseURL: `http://localhost:${config.port}`
     });
 
     await app.start();
