96df46e6b06d0357030fae23b6a3d9044ba5b3c5
ITViking <philip.dein@gmail.com>
2019-06-26 15:35:28 +0200

    fix routing

--- src/forms/app.js
+++ src/forms/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const createFormEndpoint = require('./create/endpoint')
+const createFormEndpoint = require('./create/endpoint');
+const getFormEndpoint = require("./get/endpoint");
 
 module.exports = (formsRepository) => {
     let app = express();
     app.post('/', createFormEndpoint(formsRepository));
-    app.get('/:id', createFormEndpoint(formsRepository));
+    app.get('/:id', getFormEndpoint(formsRepository));
 
     return app;
 };
