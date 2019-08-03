2ee1120c2121ed9599058ad041c22ccbb12ca865
ITViking <philip.dein@gmail.com>
2019-08-03 17:42:08 +0200

    remove unused code

--- src/start.js
+++ src/start.js
@@ -6,9 +6,3 @@ let app = createApp(config);
 smacker.start({ service: app });
 
 
-// (async function() {
-//     let app = createApp(config.port, { db, users, forms, huts });
-//     await app.start();
-//     await app.stop();
-// })();
-
