1f4789e88e9cc8de76106cbd82dcd3d9fb636a48
ITViking <philip.dein@gmail.com>
2019-08-13 11:42:17 +0200

    verify incoming tokens

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,6 +2,7 @@ let jwt = require("./jwt");
 
 module.exports = async (req, res, next) => {
     let token = req.cookies["access_token"];
+    let tokenVerification = jwt.verify(token);
     if (!token) return res.sendStatus(401);
     req.auth = { token };
     next();
