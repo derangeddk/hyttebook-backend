c9af9ce757516c87a7b92ef9db7343de8cf081cc
ITViking <philip.dein@gmail.com>
2019-08-15 13:43:26 +0200

    change the returned status code while developing, to let developers know that they havent logged in

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ let jwt = require("./jwt");
 module.exports = async (req, res, next) => {
     let token = req.cookies["access_token"];
     let tokenVerification = jwt.verify(token);
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.sendStatus(403);
     req.auth = { token };
     next();
 };
