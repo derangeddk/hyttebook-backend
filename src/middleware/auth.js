13adfc028d14a39ec53798825d1996b8805e4b36
ITViking <philip.dein@gmail.com>
2019-08-31 23:02:09 +0200

    remove logging statement

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,6 +2,7 @@ let jwt = require("./jwt");
 
 module.exports = async (req, res, next) => {
     let token = req.cookies["access_token"];
+    console.log(token);
     let tokenVerification = jwt.verify(token);
     if (!token) return res.sendStatus(403);
     req.auth = { token };
