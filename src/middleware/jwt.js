d86232ecb045f2089de3c7f9fad7aa9d1d6019ad
ITViking <philip.dein@gmail.com>
2019-09-24 01:09:55 +0200

    remove logging statements

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,18 +1,14 @@
 let jwt = require('jsonwebtoken');
 
-
 function sign(userId) {
     let token = jwt.sign({ user_id: userId }, "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!", { algorithm: "HS512", expiresIn: 30 * 60 * 1000, issuer: "hytteindex.dk" });
-    console.log("token generated: ", token);
     return token;
 }
 
 function verify(token) {
     let decodedToken = jwt.verify(token, "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!");
-    console.log("token decoded, this is the payload: ", decodedToken);
     return decodedToken;
 }
-
 
 module.exports = {
     sign,
