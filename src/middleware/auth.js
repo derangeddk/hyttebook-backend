9cfb6004cf3186cf3f1d07e9fcfff6f733975fe9
ITViking <philip.dein@gmail.com>
2019-09-24 01:09:06 +0200

    Enable Cucumber tests to carry tokens while testing.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,22 @@
 let jwt = require("./jwt");
 
 module.exports = async (req, res, next) => {
-    let token = req.cookies["access_token"];
-    console.log(token);
-    let tokenVerification = jwt.verify(token);
+    let token = req.cookies["access_token"] ? req.cookies["access_token"] : removeCookieFlags(req.headers.access_token);
     if (!token) return res.sendStatus(403);
-    req.auth = { token };
+    try{
+        req.auth = jwt.verify(token);
+    } catch(error) {
+        res.sendStatus(400);
+    }
     next();
+};
+
+//This function's sole purpose is to aid in Cucumber tests where tokens are sent in cookies.
+//For some unknown reason cooki-parser is not able to do it's work when doing Cucumber tests.
+function removeCookieFlags(token) {
+    let match = token.match(/(\;).*$/);
+    if (!match) return;
+    let cleanedToken = token.slice(0,match.index);
+
+    return cleanedToken;
 };
