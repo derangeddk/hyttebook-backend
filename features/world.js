f9cac4ddfa4a3bd2e6664a3ff8757933bdb50172
ITViking <philip.dein@gmail.com>
2019-09-24 01:14:24 +0200

    configure Cucumber tests to run in Docker. Allow tests to carry a JWT around

--- features/world.js
+++ features/world.js
@@ -2,6 +2,11 @@ const { setWorldConstructor, Before, After } = require("cucumber");
 const axios = require("axios");
 const createApp = require("../src/app");
 const config = require("config")
+//change host of db because it is not running in docker in this test.
+//Might need to changed when testing inside of containers!
+config.postgres.host = "localhost";
+//Change port because the the running dockerized app uses 4752
+config.port = 4751;
 const app = createApp(config);
 
 function World() {
@@ -15,11 +20,12 @@ Before(async function(testcase) {
         baseURL: `http://localhost:${config.port}`,
     });
 
-    this.setUserId = (userId) => {
+    this.setUser = (token) => {
         this.client = axios.create({
             baseURL: `http://localhost:${config.port}`,
             headers:{
-                "user-id": this.userId
+                'Content-type': 'application/json',
+                'access_token': `${token}`,
             }
         });
     };
