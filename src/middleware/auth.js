1c33ac064d185254ee6729c7060505a4efb173ed
ITViking <philip.dein@gmail.com>
2019-09-24 21:36:20 +0200

    rename to reflect what the variable really contains

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 let jwt = require("./jwt");
 
 module.exports = async (req, res, next) => {
+    console.log(req.cookies);
     let token = req.cookies["access_token"] ? req.cookies["access_token"] : removeCookieFlags(req.headers.access_token);
     if (!token) return res.sendStatus(403);
     try{
@@ -14,9 +15,9 @@ module.exports = async (req, res, next) => {
 //This function's sole purpose is to aid in Cucumber tests where tokens are sent in cookies.
 //For some unknown reason cooki-parser is not able to do it's work when doing Cucumber tests.
 function removeCookieFlags(token) {
-    let match = token.match(/(\;).*$/);
-    if (!match) return;
-    let cleanedToken = token.slice(0,match.index);
+    let tokenMatches = token.match(/(\;).*$/);
+    if (!tokenMatches) return;
+    let cleanedToken = token.slice(0,tokenMatches.index);
 
     return cleanedToken;
 };
