edfb1f1d37756593bc8191b65088573691529d66
ITViking <philip.dein@gmail.com>
2019-09-25 14:55:03 +0200

    enable updating forms in database

--- src/forms/repository.js
+++ src/forms/repository.js
@@ -4,8 +4,22 @@ module.exports = function constructor(db) {
     return {
         initialize: async () => await ensureFormsTableExists(db),
         find: (hutId) => findForm(db, hutId),
+        update: (hutId, formConfigs) => updateForm(db, hutId, formConfigs),
     };
 };
+
+async function updateForm(db, hutId, formConfigs) {
+    try {
+        await db.query(
+            'UPDATE forms SET data = $1 WHERE hutId = $2',
+            [formConfigs, hutId]
+            );
+    } catch(error) {
+        console.log(error);
+        throw new Error(`queried for a form with an id of ${hutId}`);
+    }
+    return;
+}
 
 async function findForm(db, hutId) {
     let result;
