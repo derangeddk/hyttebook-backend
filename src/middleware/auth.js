2754cf26a49dbf72ef6854ce800bebc7cbb8633a
ITViking <philip.dein@gmail.com>
2019-10-05 00:13:16 +0200

    typo

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,6 +3,7 @@ let jwt = require("./jwt");
 module.exports = async (req, res, next) => {
     let token = req.cookies["access_token"] ? req.cookies["access_token"] : removeCookieFlags(req.headers.access_token);
     if (!token) return res.sendStatus(403);
+    
     try{
         req.auth = jwt.verify(token);
     } catch(error) {
@@ -12,7 +13,7 @@ module.exports = async (req, res, next) => {
 };
 
 //This function's sole purpose is to aid in Cucumber tests where tokens are sent in cookies.
-//For some unknown reason cooki-parser is not able to do it's work when doing Cucumber tests.
+//For some unknown reason cookie-parser is not able to do it's work when doing Cucumber tests.
 function removeCookieFlags(token) {
     let tokenMatches;
     try {
