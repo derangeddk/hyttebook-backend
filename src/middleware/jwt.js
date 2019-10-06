617708cf4cd5ac3e362a72b119620ae50475b9cc
ITViking <philip.dein@gmail.com>
2019-10-06 14:07:40 +0200

    restructure for readability

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,12 +1,19 @@
 let jwt = require('jsonwebtoken');
+const SECRET = "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!";
 
-function sign(userId) {
-    let token = jwt.sign({ user_id: userId }, "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!", { algorithm: "HS512", expiresIn: 30 * 60 * 1000, issuer: "hytteindex.dk" });
+function sign(userId, hutId) {
+    let token = jwt.sign({ 
+        user_id: userId,
+        hut_id: hutId,
+        algorithm: "HS512", 
+        expiresIn: 30 * 60 * 1000, 
+        issuer: "hytteindex.dk"
+     }, SECRET);
     return token;
 }
 
 function verify(token) {
-    let decodedToken = jwt.verify(token, "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!");
+    let decodedToken = jwt.verify(token, SECRET);
     return decodedToken;
 }
 
