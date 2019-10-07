9a45f49ee69d5e94b50e7cd8bd9ec0c95754e0b9
ITViking <philip.dein@gmail.com>
2019-10-07 13:14:53 +0200

    jwt should only hold sesssion and not grow linearly

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,10 +1,9 @@
 let jwt = require('jsonwebtoken');
 const SECRET = "d3r4ng3d_3r-3n_k0nsu13nt-virks0mh3d!";
 
-function sign(userId, hutId) {
+function sign(userId) {
     let token = jwt.sign({ 
         user_id: userId,
-        hut_id: hutId,
         algorithm: "HS512", 
         expiresIn: 30 * 60 * 1000, 
         issuer: "hytteindex.dk"
