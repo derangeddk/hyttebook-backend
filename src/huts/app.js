e7b274cb5c5743c958ff96c695469235f0238f12
ITViking <philip.dein@gmail.com>
2019-10-07 15:19:04 +0200

    routing

--- src/huts/app.js
+++ src/huts/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const createHutsEndpoint = require('./create/endpoint');
 const getHutsEndpoint = require('./get/endpoint');
+const listHutsEndpoint = require("./list/endpoint");
 
 module.exports = (hutsRepository) => {
     let app = express();
     app.post("/", createHutsEndpoint(hutsRepository));
     app.get("/:id", getHutsEndpoint(hutsRepository));
+    app.get("/", listHutsEndpoint(hutsRepository))
 
     return app;
 };
