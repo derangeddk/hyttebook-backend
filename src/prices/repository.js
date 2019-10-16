ae318309d97895f5d0099f0c48dbc5800551bd6c
Liv Hartoft Borre <livb@itu.dk>
2019-10-16 09:21:50 +0200

    corrected comment describing price

--- src/prices/repository.js
+++ src/prices/repository.js
@@ -3,15 +3,17 @@ const uuid = require('uuid');
 /**
  * Price {
  *     id:  uuid
- *     createdAt: ISO8601string
- *     updatedAt: ISO8601string
- *     mon: number
- *     tue: number
- *     wed: number
- *     thu: number
- *     fri: number
- *     sat: number
- *     sun: number
+ *     data {
+ *         createdAt: ISO8601string
+ *         updatedAt: ISO8601string
+ *         mon: number
+ *         tue: number
+ *         wed: number
+ *         thu: number
+ *         fri: number
+ *         sat: number
+ *         sun: number
+ *     }
  * }
  */
 module.exports = function constructor(db) {
