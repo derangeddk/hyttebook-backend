2444473aa5c1639f2507d93d642567ccc3c0f55c
ITViking <philip.dein@gmail.com>
2019-10-17 16:57:29 +0200

    if no token was found return 403

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,10 @@
 let jwt = require("./jwt");
 
 module.exports = async (req, res, next) => {
+    console.log("here's the req: ", req.cookies);
     let token = req.cookies["access_token"] ? req.cookies["access_token"] : removeCookieFlags(req.headers.access_token);
     if (!token) return res.sendStatus(403);
-    
+
     try{
         req.auth = jwt.verify(token);
     } catch(error) {
@@ -15,6 +16,7 @@ module.exports = async (req, res, next) => {
 //This function's sole purpose is to aid in Cucumber tests where tokens are sent in cookies.
 //For some unknown reason cookie-parser is not able to do it's work when doing Cucumber tests.
 function removeCookieFlags(token) {
+    if(!token) return;
     let tokenMatches;
     try {
         tokenMatches = token.match(/(\;).*$/);
