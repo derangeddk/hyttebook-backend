db2d6aadb684ab86374d07ecccbd8d03cfc02cb4
Liv Hartoft Borre <livb@itu.dk>
2020-01-14 16:03:34 +0100

    added creation and finding of prices to hutsrepo instead of pricesrepo

--- src/huts/repository.js
+++ src/huts/repository.js
@@ -6,6 +6,7 @@ module.exports = function constructor(db) {
         initialize: async () => {
             await ensureHutsTableExists(db);
             await ensureRoleConnectionsTableExists(db);
+            await ensurePricesTableExists(db);
         },
         create: (hut, userId) => createHutWithImplicitFormAndAdmin(db, hut, userId),
         find: (hutId) => findHut(db, hutId),
@@ -52,15 +53,33 @@ Form {
         stdInformation: string
     }
 } 
+
+Price {
+    id:  uuid
+    data {
+        createdAt: ISO8601string
+        updatedAt: ISO8601string
+        mon: number
+        tue: number
+        wed: number
+        thu: number
+        fri: number
+        sat: number
+        sun: number
+    }
+}
+
 */
 
 async function findHut(db, hutId) {
     let result;
+    let priceResult;
     try {
         result = await db.query(
             `SELECT * FROM huts
                 WHERE id = '${hutId}'`
         )
+        priceResult = findPrice(db, result.rows[0].data.priceId);
     } catch(error) {
         throw new Error("tried to find a hut", error);
     }
@@ -73,11 +92,13 @@ async function findHut(db, hutId) {
         city: result.rows[0].data.city,
         zipCode: result.rows[0].data.zipCode,
         email: result.rows[0].data.email,
-        phone: result.rows[0].data.phone
+        phone: result.rows[0].data.phone,
+        price: priceResult.data
     }
 
     return hut;
 };
+
 
 async function createHutWithImplicitFormAndAdmin(db, hut, userId) {
     let hutId = uuid.v4();
@@ -93,6 +114,10 @@ async function createHut(db, hut, hutId) {
     let now = (new Date()).toISOString();
 
     try {
+        let priceId = await createPrice(db, hut.price);
+        delete hut["price"];
+        hut.priceId = priceId
+
         await db.query(
             `INSERT INTO huts(
                 id,
@@ -225,4 +250,78 @@ async function ensureHutsTableExists(db) {
     } catch(error) {
         throw new Error("failed while trying to create 'huts' table", error);
     }
+};
+
+
+
+// PRICES
+async function ensurePricesTableExists(db) {
+    if(await tableExists(db, "prices")) {
+        return;
+    }
+
+    try {
+        await db.query(
+            `CREATE TABLE prices(id uuid UNIQUE PRIMARY KEY, data json NOT NULL)`
+        );
+    } catch(error) {
+        throw new Error("failed to create 'prices' table", error);
+    }
+};
+
+async function findPrice(db, priceId) {
+    let result;
+    try {
+        result = await db.query(
+            `SELECT * FROM prices
+                WHERE id = '${priceId}'`
+        )
+    } catch(error) {
+        throw new Error("tried to find a price", error);
+    }
+
+    let price = {
+        id: result.rows[0].id,
+        data: {
+            monday: result.rows[0].data.monday,
+            tuesday: result.rows[0].data.tuesday,
+            wednesday: result.rows[0].data.wednesday,
+            thursday: result.rows[0].data.thursday,
+            friday: result.rows[0].data.friday,
+            saturday: result.rows[0].data.saturday,
+            sunday: result.rows[0].data.sunday
+        }
+    }
+
+    return price;
+};
+
+async function createPrice(db, hutPrice) {
+    let id = uuid.v4();
+    let now = (new Date()).toISOString();
+
+    try {
+        await db.query(
+            `INSERT INTO prices(
+                id,
+                data
+            )
+            VALUES(
+                $1::uuid,
+                $2::json
+            )`,
+            [
+                id,
+                {
+                    createdAt: now,
+                    updatedAt: now,
+                    ...hutPrice
+                }
+            ]
+        );
+    } catch(error) {
+        throw new Error("failed to insert a new price into 'prices' table");
+    }
+    
+    return id;
 };
